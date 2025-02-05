c31880b681caaf0d964cc75304d95a8dfff43fe0
Jhoan Zamora <jhoan.zamora@wizeline.com>
2025-02-05 12:51:21 -0500

    Selectors

---
+++ tests/m3-core-concepts/_1test-runner.test.ts
@@ -0,0 +1,5 @@
+import { test } from "@playwright/test";
+
+test("Test Title 1", async () => {});
+
+test("Test Title 2", async () => {});
---
+++ tests/m4-locators/_3locators-filters.test.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+test("Filtering demo", async ({ page }) => {
+  await page.goto("/savings.html");
+
+  const rows = page.getByRole("row");
+
+  console.log("rows", await rows.count());
+
+  const row = page.getByRole("row").filter({ hasText: "Competition" });
+
+  console.log(await row.textContent());
+
+  const cell = page
+    .getByRole("row")
+    .filter({ hasText: "Competition" })
+    .getByRole("cell")
+    .nth(1);
+
+  console.log(await cell.textContent());
+});
