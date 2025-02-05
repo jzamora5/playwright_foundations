9bdd74fac860da67a343e248ff69ae5ea9b39efa
Jhoan Zamora <jhoan.zamora@wizeline.com>
2025-02-05 10:21:15 -0500

    Foundation

--- tests/m2-setup/_0setup_verification.js
+++
@@ -1,7 +0,0 @@
-const { chromium } = require('@playwright/test');
-
-(async function firstScript() {
-  const browser = await chromium.launch();  
-  await browser.close();
-  console.log("we reached this line");
-})();
